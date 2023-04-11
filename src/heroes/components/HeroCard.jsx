d4cdf52184c2bb4f7329ec446de334af15d56a78
Steven Angel <stevenangel.ing@gmail.com>
2023-04-11 09:05:09 -0500

    correccion img

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -15,7 +15,7 @@ export const HeroCard = ({
     characters,
  }) => {
 
-    const heroImageUrl = `/public/heroes/${ id }.jpg`;
+    const heroImageUrl = `/assets/heroes/${ id }.jpg`;
 
     // const  charactersByHero = <p>{ characters }</p>;
 
