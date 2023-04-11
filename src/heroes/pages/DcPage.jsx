76330242015754d83d5fb6057604edc216a59645
Steven Angel <stevenangel.ing@gmail.com>
2023-04-11 08:41:59 -0500

    First commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,14 @@
+import { HeroList } from "../components"
+
+
+export const DcPage = () => {
+  return (
+    <>
+      <h1 className="mt-3">Dc Comics</h1>
+      <hr />
+
+      <HeroList publisher='DC Comics'/>
+
+    </>
+  )
+}
